72ee260874a14cc7a138df57c3756474baa80641
MrEclipse <karandachik09@gmail.com>
2025-10-15 13:23:43 +0400

    fix: remove double indexing if id columt at models.py

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class Base(DeclarativeBase):
 class Application(Base):
     __tablename__ = "applications"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     user_name = Column(String(200), nullable=False, index=True)
     description = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
